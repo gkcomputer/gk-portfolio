7f7b108d44e47dac36bde8428e0ca7b53d92a834
Krishna <mvgeethakrishna96@gmail.com>
2023-11-08 22:12:32 +0530

    projects added

--- src/pages/portfolio/experience/TimelineElements.js
+++ src/pages/portfolio/experience/TimelineElements.js
@@ -3,34 +3,35 @@ let protimelineElements = [
     id: 1,
     title: "React Developer",
     company: "TESCO-MOBILE UK",
-    location: "Individual Consultant",
+    location: "CAPGEMINI",
     description:
       "This is admin dashboard which collects the inforamtion of  signal strength of there users and Converting data to a graphical interface, through use of HTML, CSS, and JavaScript,React,Redux. ",
     buttonText: "",
-    date: "Aug - 2021 | Present",
+    date: "JUN - 2022 | MAR - 2023",
     icon: "work",
   },
   {
     id: 2,
     title: "Software Engineer",
-    location: "Individual Consultant",
-    company: "Trulia",
+    location: "KEYSTACK",
+    company: "TRULIA",
     description:
       "Trulia is an American online real estate marketplace. It facilitates buyers and renters to find homes and neighborhoods across the United States through recommendations, local insights, and map overlays that offer details on commute, schools, churches and nearby businesses.",
     buttonText: "",
-    date: "Jul - 2020 | Aug - 2021",
+    date: "FEB - 2020 | MAR - 2022",
     icon: "work",
   },
-  // {
-  //   id: 3,
-  //   title: "Frontend Developer",
-  //   location: "Freelancer",
-  //   description:
-  //     "Assessing the quality of specifications and technical design documents in order to ensure timely, relevant and meaningful feedback.",
-  //   buttonText: "",
-  //   date: "Feb-2023 - Present",
-  //   icon: "work",
-  // },
+  {
+    id: 3,
+    title: "Software Engineer",
+    location: "KEYSTACK",
+    company: "HP PRINT SERVICES",
+    description:
+      "HP printers and services project aimed to create a comprehensive platform       that offered a wide range of printer-related services, encompassing troubleshooting, printing, computing, software, and driver support.",
+    buttonText: "",
+    date: "APR - 2019 | FEB - 2020",
+    icon: "work",
+  },
 ];
 
 export default protimelineElements;
